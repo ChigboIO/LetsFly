5764e6b8deae950e6b4f037464e5c382caab3fee
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2015-12-13 10:46:11 +0100

    Add User Authentication
    Why?
    Due to the need to remeber user's information
    it is neccessary they have an account and their operations are stored
    agains this account so they can always come back to it
    
    How?
    Generate user model
    Generate users controller
    Create the forms for signup and login
    Implement the signup and login features
    Write some test
    
    Completing "Add User Authentication" feature
    https://www.pivotaltracker.com/story/show/110027252
    [finished #110027252]
    
    Fix the typo in user modela validates and fix route violatons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: "pages#index"
 
+  get "signup", to: "users#new", as: :signup
+  get "login", to: "sessions#new", as: :login
+  get "logout", to: "sessions#destroy", as: :logout
+
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
+
   # shallow do
   resources :flights, only: [:index] do
     collection do
