4a5cec5bd5aa31e4c2cbe932cb070a2a880fe5f1
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2015-12-15 21:20:45 +0100

    Implement Manage Bookings
    Why?
    If a traveler has booked flight, he or she should be able to put the
    booking the booking number that given to him and and search for the
    booking. The passenger should be able to modify the informations in the
    booking and save it.
    
    How?
    Create the view and the form to search for the booking
    add the search action in the booking controller
    add the route for the the form and the search
    
    finishing feature "Manage booking" feature
    https://www.pivotaltracker.com/story/show/110029954
    [finished #110029954]

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,6 +22,42 @@ class BookingsController < ApplicationController
     end
   end
 
+  def manage
+  end
+
+  def search
+    @booking = Booking.find_by_booking_number(params[:booking_number])
+    if @booking.blank?
+      redirect_to :back, notice: "No booking with that Number was found."
+    else
+      redirect_to edit_booking_path(@booking.id)
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    @flight = @booking.flight
+    render :book
+  end
+
+  def update
+    booking = Booking.find(params[:id])
+    booking.update(booking_params)
+    ConfirmationMailer.thank_you(
+      booking.id,
+      "Updated Passengers' Information"
+    ).deliver_now
+    
+    redirect_to :back
+  end
+
+  def destroy
+    booking = Booking.find(params[:id])
+    # pry.binding
+    booking.release_seats
+    redirect_to :back
+  end
+
   def checkout
     @booking = Booking.find(params[:booking_id])
   end
@@ -31,7 +67,11 @@ class BookingsController < ApplicationController
 
     # perform payment operations here and redirect to confirmation page
     Booking.find(booking_id).update(paid: true)
-    ConfirmationMailer.thank_you(booking_id).deliver_now
+    ConfirmationMailer.thank_you(
+      booking_id,
+      "Thank You For Your Patronage"
+    ).deliver_now
+
     redirect_to confirmation_bookings_path(booking_id)
   end
 
@@ -42,7 +82,7 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(
       :flight_id,
-      passengers_attributes: [:passenger_id, :name, :email, :phone]
+      passengers_attributes: [:id, :passenger_id, :name, :email, :phone]
     )
   end
 end
--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,7 +1,7 @@
 class ConfirmationMailer < ApplicationMailer
-  def thank_you(booking_id)
+  def thank_you(booking_id, subject)
     @booking = Booking.find(booking_id)
     @user = @booking.user_or_passenger
-    mail(to: @user.email, subject: "Thank You For Your Patronage")
+    mail(to: @user.email, subject: subject)
   end
 end
