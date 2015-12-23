5a6c162b57acd873b678f0ab1696b323b386b9a1
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2015-12-23 13:38:12 +0100

    Write ReadMe
    Why?
    The users and contributors should be able to see the description
    
    How?
    Create the Readme.md file
    write the content of the file
    
    https://www.pivotaltracker.com/story/show/110645572
    [finished #110645572]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: {
+  validates :email, presence: true, uniqueness: true, format: {
     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
   }
 end
